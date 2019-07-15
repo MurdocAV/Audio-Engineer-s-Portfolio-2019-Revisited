56b508034953e99dc31d5c1e3cf6889d303b6a2d
MurdocAV <azaan.virk1@gmail.com>
2019-07-15 13:58:06 +1200

    changed app component to class component -->update to stateful

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,16 +3,16 @@ import Discography from './Discography/Discography.jsx'
 import Header from './Header.jsx'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
-function Main (props) {
-  // eslint-disable-next-line no-console
-  // console.log('Logging props in main:', props)
-
-  return (
-    <Router>
-      <Header />
-      <Discography />
-    </Router>
-  )
+class App extends React.Component {
+// Stateful component
+  render() {
+    return (
+      <Router>
+        <Header />
+        <Discography />
+      </Router>
+    )
+  }
 }
 
-export default Main
+export default App
