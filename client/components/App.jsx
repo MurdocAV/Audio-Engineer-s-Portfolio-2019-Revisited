299e9595e694474ccfad3f353b38f1307f57673d
MurdocAV <azaan.virk1@gmail.com>
2019-07-19 09:14:12 +1200

    Using Routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Discography from './Discography/Discography'
-import Home from './Home/Home'
-import Contact from './Contact/Contact'
 import Header from './Header'
 import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import Discography from './Discography/Discography'
+import Home from './Home/Home'
+import Contact from './Contact/Contact'
+import {Link, Route} from 'react-router-dom'
 
 function Header (props) {
   return (
@@ -10,8 +13,13 @@ function Header (props) {
       <div className="header">
       </div>
       <div className="nav">
-        <h2 className="home"><Link to={'/'}>Home</Link></h2>
-        <h2 className="discography active"><Link to={'/'}>Discography</Link></h2>
+      <Switch>
+      <h2 className="home"><Route exact path='/Home' component={Home} /></h2>
+            <Route exact path='/Discography' component={Discography} />
+            <Route exact path='/Contact' component={Contact} />
+      </Switch>
+        <h2 className="home"><Route to={'/Home'}>Home</Link></h2>
+        <h2 className="discography active"><Route to={'/'}>Discography</Link></h2>
         <h2 className="contact"><Link to={'/'}>Contact</Link></h2>
       </div>
     </React.Fragment>
