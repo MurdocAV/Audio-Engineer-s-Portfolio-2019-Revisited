157ee42ee6f959e6d2caa7dadd8f38e5aa0486d0
MurdocAV <azaan.virk1@gmail.com>
2019-07-24 16:46:50 +1200

    new components set-up

---
+++ client/components/Welcome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Router from 'react-router-dom'
+
+class Welcome extends React.Component {
+    
+  render() {
+    return (
+      <Router>
+        {/* Add route to Discography with animations on the welcome screen */}
+      </Router>
+    )
+  }
+}
+
+export default Welcome
