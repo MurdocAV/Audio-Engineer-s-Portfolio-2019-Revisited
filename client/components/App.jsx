ce7b343988dd6fb7f95105a0c00fb6cbc2a9fe7b
MurdocAV <azaan.virk1@gmail.com>
2019-07-27 13:55:06 +1200

    Working modal in the welcome component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header'
 import Discography from './Discography/Discography'
 import Home from './Home/Home'
 import Contact from './Contact/Contact'
+import Welcome from './Welcome'
 import {HashRouter as Router, Route} from 'react-router-dom'
 
 class App extends React.Component {
@@ -11,7 +12,7 @@ class App extends React.Component {
     return (
       <Router>
         <Header />
-        <Route exact path="/" component={Discography} />
+        <Route exact path="/" component={Welcome} />
         <Route exact path="/Discography" component={Discography} />
         <Route exact path="/Home" component={Home} />
         <Route exact path="/Contact" component={Contact} />
--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,15 +1,57 @@
 import React from 'react'
-import Router from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
-class Welcome extends React.Component {
-    
-  render() {
-    return (
-      <Router>
-        {/* Add route to Discography with animations on the welcome screen */}
-      </Router>
-    )
+class StaticModal extends React.Component {
+  state = {
+    show: false
   }
+
+  setShow (boolean) {
+    // sets the show value to the given boolean param
+    this.setState({
+      show: boolean
+    })
+  }
+
+  example() {
+  
+    return (
+      <React.Fragment>
+        <Button variant="primary" onClick={() => this.setShow(true)}>
+          Custom Width Modal
+        </Button>
+  
+        <Modal
+          show={this.state.show}
+          onHide={() => this.setShow(false)}
+          dialogClassName="modal-90w"
+          aria-labelledby="example-custom-modal-styling-title"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="example-custom-modal-styling-title">
+              Custom Modal Styling
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+              commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+              ipsam atque a dolores quisquam quisquam adipisci possimus
+              laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+              accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+              reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+              deleniti rem!
+            </p>
+          </Modal.Body>
+        </Modal>
+      </React.Fragment>
+    );
+  }
+  
+  render () {
+    return(this.example())
+  };
 }
 
-export default Welcome
+export default StaticModal
