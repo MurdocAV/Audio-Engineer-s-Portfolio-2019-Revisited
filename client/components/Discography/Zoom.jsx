7719bf703ded7788a60d8c01efc2076191e0eafe
MurdocAV <azaan.virk1@gmail.com>
2019-07-29 15:06:12 +1200

    added scss

--- client/components/Discography/Zoom.jsx
+++ client/components/Discography/Zoom.jsx
@@ -2,53 +2,52 @@ import React from 'react'
 import Modal from 'react-bootstrap/Modal'
 
 class Zoom extends React.Component {
-  state = {
-    show: false
-  }
+    state = {
+        show: false
+    }
 
-  setShow (boolean) {
-    // sets the show value to the given boolean param
-    this.setState({
-      show: boolean
-    })
-  }
+    setShow(boolean) {
+        // sets the show value to the given boolean param
+        this.setState({show: boolean})
+    }
 
-  example() {
-  
-    return (
-      <React.Fragment>
-        {console.log(this.props.info)}
-        <img className='albumLink albumArt' src={this.props.src} onClick={() => 
-          this.setShow(true)} />
+    example() {
 
-        <Modal
-          size="xl"
-          show={this.state.show}
-          onHide={() => this.setShow(false)}
-          centered={true} 
-          dialogClassName="modal-90w"
-          aria-labelledby="example-custom-modal-styling-title"
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="example-custom-modal-styling-title">
-              {this.props.info.albumName}
-            </Modal.Title>
-          </Modal.Header>
+        return (
+            <React.Fragment>
+                {console.log(this.props.info)}
+                <img
+                    className='albumLink albumArt'
+                    src={this.props.src}
+                    onClick={() => this.setShow(true)}/>
 
-          <Modal.Body>
-            <img className='albumModal' src={this.props.src} alt=""/>
-            {/* {TODO: Create another component to see the track list} */}
-            <p>Track List...</p>
-          </Modal.Body>
+                <Modal
+                    size="xl"
+                    show={this.state.show}
+                    onHide={() => this.setShow(false)}
+                    centered={true}
+                    dialogClassName="modal-90w"
+                    aria-labelledby="example-custom-modal-styling-title">
+                    <Modal.Header closeButton>
+                        <Modal.Title id="example-custom-modal-styling-title">
+                            {this.props.info.albumName}
+                        </Modal.Title>
+                    </Modal.Header>
 
-        </Modal>
-      </React.Fragment>
-    )
-  }
-  
-  render () {
-    return(this.example())
-  }
+                    <Modal.Body>
+                        <img className='albumModal' src={this.props.src} alt=""/> 
+                        { /* {TODO: Create another component to see the track list} */}
+                        <p>Track List...</p>
+                    </Modal.Body>
+
+                </Modal>
+            </React.Fragment>
+        )
+    }
+
+    render() {
+        return (this.example())
+    }
 }
 
 export default Zoom
