aaf2da811469dc084d8838605a3e4c0f154f4f88
MurdocAV <azaan.virk1@gmail.com>
2019-12-10 14:14:26 +1300

    Changed Link class

--- client/components/Parts/Navigation.jsx
+++ client/components/Parts/Navigation.jsx
@@ -10,17 +10,17 @@ class Navigation extends React.Component {
                     <h1 className="titleHeader">Azaan Virk</h1>
                 </div>
                 <div className="nav">
-                    <NavLink className="homeLink" activeClassName="active" to={'/Home'}>Home</NavLink>
+                    <NavLink className="home Link" activeClassName="active" to={'/Home'}>Home</NavLink>
                     <NavLink
-                        className="aboutLink"
+                        className="about Link"
                         activeClassName="active"
                         to={'/About'}>About</NavLink>
-                    <NavLink className="projectsLink" activeClassName="active" to={'/Projects'}>Projects</NavLink>
-                    <NavLink className="exampleFormLink" activeClassName="active" to={'/ExampleForm'}>Example Form</NavLink>
-                    <NavLink className="referencesLink" activeClassName="active" to={'/References'}>References</NavLink>
-                    <NavLink className="emailLink" activeClassName="active" to={'/Email'}>Email</NavLink>
-                    <NavLink className="hiringLink" activeClassName="active" to={'/Hiring'}>Hiring</NavLink>
-                    <NavLink className="documentationLink" activeClassName="active" to={'/Documentation'}>Documentation</NavLink>
+                    <NavLink className="projects Link" activeClassName="active" to={'/Projects'}>Projects</NavLink>
+                    <NavLink className="exampleForm Link" activeClassName="active" to={'/ExampleForm'}>Example Form</NavLink>
+                    <NavLink className="references Link" activeClassName="active" to={'/References'}>References</NavLink>
+                    <NavLink className="email Link" activeClassName="active" to={'/Email'}>Email</NavLink>
+                    <NavLink className="hiring Link" activeClassName="active" to={'/Hiring'}>Hiring</NavLink>
+                    <NavLink className="documentation Link" activeClassName="active" to={'/Documentation'}>Documentation</NavLink>
                 </div>
             </div>
         )
