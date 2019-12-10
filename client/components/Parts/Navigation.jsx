d7aaebced061947f932671f703fc20629f26a7ca
MurdocAV <azaan.virk1@gmail.com>
2019-12-10 14:11:23 +1300

    New Nav for exporting to murdocav.github.io

--- client/components/Parts/Navigation.jsx
+++ client/components/Parts/Navigation.jsx
@@ -7,15 +7,20 @@ class Navigation extends React.Component {
         return (
             <div className="header">
                 <div className="title">
-                    <h1 className="titleHeader">Name Placeholder</h1>
+                    <h1 className="titleHeader">Azaan Virk</h1>
                 </div>
                 <div className="nav">
                     <NavLink className="homeLink" activeClassName="active" to={'/Home'}>Home</NavLink>
                     <NavLink
-                        className="discographyLink"
+                        className="aboutLink"
                         activeClassName="active"
-                        to={'/Discography'}>Discography</NavLink>
-                    <NavLink className="contactLink" activeClassName="active" to={'/Contact'}>Contact</NavLink>
+                        to={'/About'}>About</NavLink>
+                    <NavLink className="projectsLink" activeClassName="active" to={'/Projects'}>Projects</NavLink>
+                    <NavLink className="exampleFormLink" activeClassName="active" to={'/ExampleForm'}>Example Form</NavLink>
+                    <NavLink className="referencesLink" activeClassName="active" to={'/References'}>References</NavLink>
+                    <NavLink className="emailLink" activeClassName="active" to={'/Email'}>Email</NavLink>
+                    <NavLink className="hiringLink" activeClassName="active" to={'/Hiring'}>Hiring</NavLink>
+                    <NavLink className="documentationLink" activeClassName="active" to={'/Documentation'}>Documentation</NavLink>
                 </div>
             </div>
         )
